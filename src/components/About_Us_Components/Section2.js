acb9d537dad4d68d9ed3de19266d0c8145dfb3eb
Adi-Elite <adityaelitemantra@gmail.com>
2021-08-23 19:49:56 +0530

    about us para change

--- src/components/About_Us_Components/Section2.js
+++ src/components/About_Us_Components/Section2.js
@@ -7,24 +7,19 @@ const Section2 = () => {
         <div className="col-md-6 col-12 d-flex justify-content-center align-items-center p-2">
           <div className="">
             <p className="generic-title-heading">CrossTower</p>
-            <p className="generic-para-text primary-color-grey">
-              Founded in 2019, CrossTower is a digital assets capital markets
-              firm founded by financial industry veterans on a mission to bring
-              digital asset trading and investing to institutional and
-              professional traders. The firm first launched a multi-asset
-              trading platform last year, and has since expanded to include a
-              capital markets desk with best-in-class services and products
-              tailored to the needs of hedge funds, family offices and other
-              markets participants. CrossTower has leveraged its vast experience
-              in trading, technology, operational infrastructure, innovative
-              pricing, regulations and compliance to open up digital assets to a
-              sophisticated audience.
+            <p className="generic-para-text primary-color-grey" style={{fontSize:"21px"}}>
+              CrossTower is a digital assets capital markets firm founded by
+              financial industry veterans on a mission to bring digital asset
+              trading and investing to retail traders. CrossTower has leveraged
+              its vast experience in trading, technology, operational
+              infrastructure, innovative pricing, regulations and compliance to
+              open up digital assets to a sophisticated audience.
             </p>
           </div>
         </div>
         <div className="col-md-6 col-12 justify-content-center align-items-center d-flex">
           <div className="parent-div-section2 justify-content-center align-items-center d-flex p-5">
-            <img src={Image2} alt="" className="w-100"/>
+            <img src={Image2} alt="" className="w-100" />
           </div>
         </div>
       </div>
